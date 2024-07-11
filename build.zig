972efe7ffb7348a1ddd1bf7ae1802dbf95a1e3f4
Tobias Simetsreiter <tobias.simetsreiter@wabtec.com>
2024-07-11 20:41:14 +0200

    add first dependency

--- build.zig
+++ build.zig
@@ -1,12 +1,26 @@
-// meta-allyourcode
+// meta_allyourcode
 //
 // by Tobias Simetsreiter <dasimmet@gmail.com>
 //
 
+const meta_allyourcode = @This();
 const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     _ = target;
     _ = optimize;
+    const dep_option = b.option([]const u8, "dependency", "the dependency to fetch");
+    if (dep_option) |d| {
+        _ = b.lazyDependency(d, .{});
+    }
 }
+
+pub const lazy = struct {
+    pub fn dependency(b: *std.Build, name: []const u8, args: anytype) ?*std.Build.Dependency {
+        const this_dep = b.dependencyFromBuildZig(meta_allyourcode, .{
+            .dependency = name,
+        });
+        return this_dep.builder.lazyDependency(name, args);
+    }
+};
--- example/build.zig
+++ example/build.zig
@@ -1,7 +1,21 @@
+// meta_allyourcode_example
+//
+// by Tobias Simetsreiter <dasimmet@gmail.com>
+//
+
+const meta_allyourcode = @import("meta_allyourcode");
+
 const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    _ = target;
-    _ = optimize;
+    const meta_dep = meta_allyourcode.lazy.dependency(
+        b,
+        "cmake",
+        .{
+            .target = target,
+            .optimize = optimize,
+        },
+    );
+    _ = meta_dep;
 }
