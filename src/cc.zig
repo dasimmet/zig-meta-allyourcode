3949bc51f97f3b3a6a01108884f7ac928f631c8a
Tobias Simetsreiter <tobias.simetsreiter@wabtec.com>
2024-07-15 09:45:43 +0200

    catch error when zig is not passed through env

--- src/cc.zig
+++ src/cc.zig
@@ -5,16 +5,33 @@ pub fn main() !void {
     return subcommand("cc");
 }
 pub fn subcommand(cmd: []const u8) !void {
+    const stderr = std.io.getStdErr();
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     var env = try std.process.getEnvMap(arena.allocator());
     var args = std.ArrayList([]const u8).init(arena.allocator());
-    try args.append(env.get("ZIG").?);
+    var zig_found = false;
+    if (env.get("ZIG")) |zig_exe| {
+        try args.append(zig_exe);
+        zig_found = true;
+    }
     try args.append(cmd);
     var p_args = std.process.args();
     var i: usize = 0;
     while (p_args.next()) |arg| : (i += 1) {
         if (i == 0) {
+            if (!zig_found) {
+                const message =
+                    \\
+                    \\Error: this executable ({s}) needs the
+                    \\ZIG environment variable pointing to the zig compiler,
+                    \\so it can run "zig {s}"
+                    \\
+                    \\
+                ;
+                try stderr.writer().print(message, .{ arg, cmd });
+                std.process.exit(1);
+            }
         } else {
             try args.append(arg);
         }
@@ -32,8 +49,7 @@ pub fn subcommand(cmd: []const u8) !void {
     try proc.spawn();
     const res = try proc.wait();
     switch (res) {
-        .Exited => 
-            std.process.exit(res.Exited),
+        .Exited => std.process.exit(res.Exited),
         .Signal => {
             std.debug.print("Signal Received: {}\n", .{res.Signal});
             unreachable;
@@ -41,7 +57,6 @@ pub fn subcommand(cmd: []const u8) !void {
         .Stopped => {
             std.debug.print("Stopped: {}\n", .{res.Stopped});
             unreachable;
-
         },
         .Unknown => {
             std.debug.print("Unknown: {}\n", .{res.Stopped});
