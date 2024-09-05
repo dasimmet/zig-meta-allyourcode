ac26a12e36cc05af29950815f45bf1074dd6e597
Tobias Simetsreiter <dasimmet@gmail.com>
2024-09-05 21:38:26 +0200

    pin zig 0.14.0-dev.1417+242d268a0

--- build.zig
+++ build.zig
@@ -17,6 +17,14 @@ pub const DefaultBuildOptions = struct {
 };
 
 pub fn build(b: *std.Build) void {
+    comptime {
+        const current_zig = builtin.zig_version;
+        const min_zig = std.SemanticVersion.parse("0.14.0-dev.1417+242d268a0") catch unreachable; // build system changes: ziglang/zig#19597
+        if (current_zig.order(min_zig) == .lt) {
+            @compileError(std.fmt.comptimePrint("Your Zig version v{} does not meet the minimum build requirement of v{}", .{ current_zig, min_zig }));
+        }
+    }
+
     const defaults: DefaultBuildOptions = .{
         .target = b.standardTargetOptions(.{}),
         .optimize = b.option(
